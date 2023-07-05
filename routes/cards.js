a28082e011404474c04a13316ac9faf582d935e5
Firsakov Dmitriy <firsakovds@mail.ru>
2023-07-05 23:41:23 +0300

    tesst

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const {celebrate, Joi} = require('celebrate');
 const {
   getCards,
   createCards,
@@ -8,7 +9,21 @@ const {
 } = require("../controllers/cards");
 router.get("/cards", getCards);
 router.post("/cards", createCards);
-router.delete("/cards/:cardId", deleteCards);
+//router.delete("/cards/:cardId", deleteCards);
+router.delete("/cards/:cardId", celebrate({
+  body: Joi.object().keys({
+    //email: Joi.string().required().email(),
+    //password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    owner: Joi.string().min(2).max(30),
+    link: Joi.string().regex(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*/)
+  }),
+}), deleteCards);
+
+
+
+
+
 router.put("/cards/:cardId/likes", likeCard);
 router.delete("/cards/:cardId/likes", dislikeCard);
 module.exports = router;
