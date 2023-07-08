df38e213639d53fbae75951010e011b01f002d4e
Firsakov Dmitriy <firsakovds@mail.ru>
2023-07-08 14:24:01 +0300

    tesst

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 //5. Сделайте мидлвэр для авторизации
-const handleAuthError = (res, req, next) => {
-  return next(new UnauthorizedError('Необходима авторизация'));
-  //res.status(401).send({ message: 'Необходима авторизация' });
-};
+
 
 const extractBearerToken = (header) => {
   return header.replace('Bearer ', '');
@@ -14,7 +11,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const token = extractBearerToken(authorization);
@@ -23,7 +20,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return handleAuthError(res);
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
