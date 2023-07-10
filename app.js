49eae2c8d1c64545712cd1b97b93d6ec494faeaf
Firsakov Dmitriy <firsakovds@mail.ru>
2023-07-10 21:44:22 +0300

    поставил localhost

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const UserNotFound = require("./errors/UserNotFound");
 const { PORT = 3000 } = process.env;
 mongoose
   //использовал .connect("mongodb://0.0.0.0:27017/mestodb", с loсalhost не было ответа от сервера
-  .connect("mongodb://0.0.0.0:27017/mestodb", {
+  .connect("mongodb://loсalhost:27017/mestodb", {
     useNewUrlParser: true,
   })
   .then(() => {
